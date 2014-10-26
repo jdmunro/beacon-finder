124fc3b52cb490d8048f06d1eb7492bd3f4a0b76
James Munro <james.munro@rnfdigital.com>
2014-10-26 17:15:48 +0000

    Added beeping based on proximity.

--- app/src/main/java/com/rnfdigital/beaconfinder/BeaconFinderActivity.java
+++ app/src/main/java/com/rnfdigital/beaconfinder/BeaconFinderActivity.java
@@ -1,6 +1,8 @@
 package com.rnfdigital.beaconfinder;
 
 import android.app.Activity;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.util.Log;
@@ -12,6 +14,8 @@ import com.estimote.sdk.Region;
 import com.estimote.sdk.Utils;
 
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class BeaconFinderActivity extends Activity {
@@ -19,19 +23,22 @@ public class BeaconFinderActivity extends Activity {
     private static final String LOG_TAG = "BeaconFinderActivity";
     private static final String UUID = "d2d27af6-fca8-4086-ac0d-3b90e4f2d372";
     private static final Region BEACON_REGION = new Region("regionId", UUID, 16629, 25543);
-    private static final long DEFAULT_SCAN_PERIOD_IN_MS = 150;
-    private static final long MIN_TIME_BETWEEN_PROXIMITY_CHANGES_IN_MS = 250;
+    private static final long DEFAULT_SCAN_PERIOD_IN_MS = 500;
+    private static final long MIN_TIME_BETWEEN_PROXIMITY_CHANGES_IN_MS = 500;
 
     private TextView beaconRangeTextView;
     private BeaconManager beaconManager;
     private Utils.Proximity lastKnownProximity = Utils.Proximity.UNKNOWN;
     private long timeOfLastProximityUpdate = 0;
+    private ToneGenerator toneGenerator;
+    private Timer beepTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beacon_finder);
         beaconRangeTextView = (TextView) findViewById(R.id.beacon_range_text);
+        toneGenerator = new ToneGenerator(AudioManager.STREAM_ALARM, 50);
         prepareForRangingBeacons();
     }
 
@@ -56,12 +63,55 @@ public class BeaconFinderActivity extends Activity {
             if (hasMinimumTimeSinceLastUpdateElapsed()) {
                 beaconRangeTextView.setText(proximity.toString());
                 lastKnownProximity = proximity;
+                handleBeaconProximityDidChange(proximity);
                 Log.d(LOG_TAG, "Updating beacon proximity: " + proximity);
             }
         }
 
         timeOfLastProximityUpdate = System.currentTimeMillis();
     }
+
+    private long calculateBeepIntervalForProximity(Utils.Proximity proximity) {
+        switch (proximity) {
+            case IMMEDIATE:
+                return 500;
+            case NEAR:
+                return 1000;
+            case FAR:
+                return 2000;
+            default:
+                return 0;
+        }
+    }
+
+    private void startBeepingWithInterval(long interval) {
+        cancelBeeping();
+        beepTimer = new Timer();
+        beepTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                toneGenerator.startTone(ToneGenerator.TONE_CDMA_ALERT_CALL_GUARD, 100);
+            }
+        }, 0, interval);
+    }
+
+    private void handleBeaconProximityDidChange(Utils.Proximity proximity) {
+        if (proximity == Utils.Proximity.UNKNOWN) {
+            cancelBeeping();
+        } else {
+            final long beepInterval = calculateBeepIntervalForProximity(proximity);
+            startBeepingWithInterval(beepInterval);
+        }
+    }
+
+    private void cancelBeeping() {
+        if (beepTimer != null) {
+            beepTimer.cancel();
+            beepTimer.purge();
+            beepTimer = null;
+        }
+    }
+
     private void prepareForRangingBeacons() {
         beaconManager = new BeaconManager(this);
         beaconManager.setRangingListener(new BeaconManager.RangingListener() {
